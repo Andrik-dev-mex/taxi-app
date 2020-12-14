6d662f4bd9c146caf2875f118a65c0018beb2e43
Andrik Pineda <kean5817@gmail.com>
2020-12-14 12:20:55 -0600

    Iniciando con la configutación del proyecto

---
+++ config/database.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+import "firebase/database";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCbbokgOHTkr9UNG90VwaS-MGyHqdmAq0o",
+  authDomain: "taxi-app-c1c9e.firebaseapp.com",
+  projectId: "taxi-app-c1c9e",
+  storageBucket: "taxi-app-c1c9e.appspot.com",
+  messagingSenderId: "590101887264",
+  appId: "1:590101887264:web:55c24812f79ce6efdd3f45",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export default {
+  firebase,
+  db,
+  googleProvider,
+};
